bfcf9e859dfcd9179da544825a165b9cf9d14050
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-02-17 14:35:05 -0700

    Added Constructors

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -6,10 +6,17 @@ public class Calculator {
     private double height;
     private String shape;
 
+    public Calculator(String shape, double width, double height) {
+        this.shape = shape;
+        this.width = width;
+        this.height = height;
+
+    }
+
     //method to calculate area of a certain shape
     public void calculateArea(String name) {
         System.out.println(name + " is " + width + " wide and " + height +
-                " tall and the area is " + (width*height) + ".");
+                " tall and the area is " + (width * height) + ".");
     }
 
     //method for setting width
@@ -17,8 +24,21 @@ public class Calculator {
         width = widthOfShape;
     }
 
+    public double getWidth() {
+        return width;
+    }
+
     //method for setting height
     public void setHeight(double heightOfShape) {
         height = heightOfShape;
     }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void describeShape() {
+        System.out.println("The " + getShape() + " is " + getWidth() + " wide, and " + getHeight() + " high.");
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Calculator rectangleShape = new Calculator();
+        Calculator rectangleShape = new Calculator("rectangle", 20, 40);
+        Calculator squareShape = new Calculator("Square", 25, 25);
 
-        rectangleShape.setHeight(40);
-        rectangleShape.setWidth(20);
+
+        //rectangleShape.setHeight(40);
+        //rectangleShape.setWidth(20);
 
         String s = "The rectangle";
         rectangleShape.calculateArea("The rectangle");
